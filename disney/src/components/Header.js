006f2bf9b154019c595813074988efbff2a36544
parvin <84388565+Parvin-Gasimli@users.noreply.github.com>
2021-11-18 23:25:26 +0300

    disney images videos and using the styled component

--- disney/src/components/Header.js
+++ disney/src/components/Header.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
 const Header = () => {
-    return (
-        <div>
-            header
-        </div>
-    )
-}
+  return <Nav>
+      <Logo src="/images/logo.svg"/>
+      <NavMenu>
+          <a>
+              <img src="/images/home-icon.svg"/>
+              <span>Home</span>
+          </a>
 
-export default Header
+      </NavMenu>
+      </Nav>;
+};
+
+export default Header;
+const Nav = styled.div`
+  height: 70px;
+  background-color: #090b13;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
+  
+`
+const Logo=styled.img`
+width: 80px;
+
+
+`
+const NavMenu=styled.div`
+
+
+`
+
+
