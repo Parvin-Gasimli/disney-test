af18fc72150b0e515f6d6d55dd51838848437105
parvin <84388565+Parvin-Gasimli@users.noreply.github.com>
2021-11-19 01:00:00 +0300

    End the Viewers

--- disney/src/components/Viewers.js
+++ disney/src/components/Viewers.js
@@ -1,15 +1,56 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
 const Viewers = () => {
-    return (
-        <div>
-            views
-            
-        </div>
-    )
+  return (
+    <Container>
+      <Wrap>
+        <img src="/images/viewers-disney.png" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-pixar.png" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-marvel.png" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-starwars.png" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-national.png" />
+      </Wrap>
+      
+    </Container>
+  );
+};
+
+export default Viewers;
+
+const Container = styled.div`
+  margin-top: 30px;
+  display: grid;
+  padding: 30px 0px 26px;
+  grid-gap: 25px;
+  grid-template-columns: repeat(5, minmax(0, 1fr));
+`;
+const Wrap = styled.div`
+cursor: pointer;
+transition: all.4s linear;
+border-radius: 10px;
+border: 3px solid rgba(249,249,249,0.1);
+box-shadow: rgb(0 0 0 /69%) 0px 26px 30px -10px,
+      rgb(0 0 0/73%) 0px 16px 10px -10px;
+
+img{
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
 }
 
-export default Viewers
+&:hover{
 
+    transform: scale(1.05);
+    border-color: rgba(249,249,249,0.8);
+}
 
-
+`;
