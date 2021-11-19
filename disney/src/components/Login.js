2bfc783978c14a9568ccc24734afc1014bbe2820
parvin <84388565+Parvin-Gasimli@users.noreply.github.com>
2021-11-19 15:09:28 +0300

    Login

---
+++ disney/src/components/Login.js
@@ -0,0 +1,82 @@
+import React from "react";
+import styled from "styled-components";
+const Login = () => {
+  return (
+    <Container>
+      <CTA>
+        <CTALogone src="/images/cta-logo-one.svg"></CTALogone>
+        <SignUp>GET ALL THERE</SignUp>
+        <Description>
+            Get Premier Access to Raya and the Last Dragon For
+            With a Disney+ subscription.As of 03/26/22, the price and The Disney
+            Bundle will increase by $1
+        </Description>
+        <CTALogotwo src="/images/cta-logo-two.png">
+
+        </CTALogotwo>
+
+      </CTA>
+    </Container>
+  );
+};
+
+export default Login;
+const Container = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+
+  /* height: calc(100vh-70px); */
+
+  &::before {
+    background-position: top;
+    background-size: cover;
+    background-repeat: no-repeat;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    content: "";
+    background-image: url("/images/login-background.jpg");
+  }
+`;
+const CTA = styled.div`
+  max-width: 650px;
+  padding: 80px 40px;
+  width: 90%;
+  z-index: 6;
+  display: flex;
+  flex-direction: column;
+  padding: 17px 0;
+`;
+const CTALogone = styled.img``;
+const SignUp = styled.a`
+    padding: 17px 0px;
+  width: 100%;
+  background-color: #0063e5;
+  font-weight: bold;
+  color: #f9f9f9;
+  border-radius: 4px;
+  text-align: center;
+  font-size: 18px;
+  transition: .3s linear;
+  letter-spacing: 1.5px;
+  cursor: pointer;
+  margin-top: 8px;
+  margin-bottom: 12px;
+  &:hover{
+      background:#0483ee ;
+  }
+`;
+const Description=styled.p`
+font-size: 11px;
+letter-spacing: 1.5px;
+text-align: center;
+line-height:1.5 ;
+
+
+`
+const CTALogotwo=styled.img``
