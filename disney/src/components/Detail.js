2c5973f1144d51899308e90b9307ef590f587019
parvin <84388565+Parvin-Gasimli@users.noreply.github.com>
2021-11-19 13:20:32 +0300

    Details page end

--- disney/src/components/Detail.js
+++ disney/src/components/Detail.js
@@ -9,35 +9,31 @@ const Detail = () => {
       </Background>
 
       <ImageTitle>
-          <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/EF737B93E2F2ABE27C74CBBEB322F18A421E7986129E9989587CEF2295B0447F/scale?width=1440&aspectRatio=1.78&format=png"/>
+        <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/EF737B93E2F2ABE27C74CBBEB322F18A421E7986129E9989587CEF2295B0447F/scale?width=1440&aspectRatio=1.78&format=png" />
       </ImageTitle>
       <Controls>
-          <PlayButton>
-              <img src="/images/play-icon-black.png"/>
-              <span>
-                  PLAY
-              </span>
+        <PlayButton>
+          <img src="/images/play-icon-black.png" />
+          <span>PLAY</span>
+        </PlayButton>
 
-          </PlayButton>
-
-          <TrailerButton>
-          <img src="/images/play-icon-white.png"/>
-              <span>
-                  TRAILER
-              </span>
-
-          </TrailerButton>
-          <AddButton>
-              <span>
-                  +
-              </span>
-
-          </AddButton>
-          <WatchButton>
-
-          </WatchButton>
-
+        <TrailerButton>
+          <img src="/images/play-icon-white.png" />
+          <span>TRAILER</span>
+        </TrailerButton>
+        <AddButton>
+          <span>+</span>
+        </AddButton>
+        <WatchButton>
+          <img src="/images/group-icon.png" />
+        </WatchButton>
       </Controls>
+      <Subtitle>2018 * 7m * Family,Kids,Animation</Subtitle>
+      <Description>
+        A Chinese mom who's sad when her grown son leaves home gets another
+        chance at motherhood when one of her dumplings springs to life.But she
+        finds that nothing stays cute and small forever
+      </Description>
     </Container>
   );
 };
@@ -60,73 +56,82 @@ const Background = styled.div`
     width: 100%;
     height: 100%;
     object-fit: cover;
-    
-
   }
 `;
-const ImageTitle =styled.div`
+const ImageTitle = styled.div`
+  width: 35vw;
 
-width: 35vw;
-
-img{
+  img {
     width: 48%;
     height: 51%;
     object-fit: cover;
     margin-left: 60px;
-    padding-top: 58px;}
+    padding-top: 58px;
+  }
+`;
+const Controls = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const PlayButton = styled.button`
+  margin-left: 40px;
+  border-radius: 4px;
+  font-size: 15px;
+  display: flex;
+  align-items: center;
+  height: 56px;
+  background-color: rgb(249, 249, 249);
+  border: none;
+  padding: 0px 24px;
+  margin-right: 22px;
+  letter-spacing: 1.8px;
+  cursor: pointer;
+  transition: 0.3s linear;
+  &:hover {
+    background: rgb(198, 198, 198);
+  }
+`;
+const TrailerButton = styled(PlayButton)`
+  background: rgba(0, 0, 0, 0.3);
+  border: 1px solid rgba(249, 249, 249);
+  color: rgba(249, 249, 249);
+`;
+const AddButton = styled.button`
+  margin-right: 16px;
+  width: 37px;
+  height: 37px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  border: 2px solid white;
+  background-color: rgba(0, 0, 0, 0.6);
+  cursor: pointer;
 
-
-`
-const Controls=styled.div`
-display: flex;
-
-
-`
-const PlayButton=styled.button`
-border-radius:4px ;
-font-size: 15px;
-display: flex;
-align-items: center;
-height: 56px;
-background-color: rgb(249,249,249);
-border: none;
-padding: 0px 24px;
-margin-right: 22px;
-letter-spacing: 1.8px;
-cursor: pointer;
-transition: .3s linear;
-&:hover{
-    background: rgb(198,198,198);
-}
-
-
-
-`
-const TrailerButton =styled(PlayButton)`
-background: rgba(0,0,0,0.3);
-border: 1px solid rgba(249,249,249);
-color: rgba(249,249,249);
-
-
-
-
-`
-const AddButton =styled.button`
-width: 44px;
-height: 44px;
-display: flex;
-align-items: center;
-justify-content: center;
-border-radius: 50%;
-border: 2px solid white;
-background-color: rgba(0,0,0,0.6);
-
-span{
+  span {
     font-size: 30px;
-    color:white;
-
-}
-
-
-`
-const WatchButton=styled.button``
+    color: white;
+  }
+`;
+const WatchButton = styled(AddButton)`
+  background: rgb(0, 0, 0);
+`;
+const Subtitle = styled.div`
+  font-size: 15px;
+  color: rgb(249, 249, 249);
+  min-height: 20px;
+  margin-top: 26px;
+  display: inline-block;
+  font-size: 15px;
+  margin-left: 22px;
+`;
+const Description = styled.div`
+  line-height: 1.4;
+  font-size: 20px;
+  margin-top: 16px;
+  line-height: 1.4;
+  font-size: 15px;
+  margin-top: 16px;
+  margin-left: 23px;
+  width: 445px;
+`;
